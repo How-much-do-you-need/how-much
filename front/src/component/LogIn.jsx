a3fe3ba45241e1ca3af5ad4be28a9b2d0990469a
inth9198 <inth2474@naver.com>
2023-01-05 09:46:36 +0900

    [Add]프론트 페이지 라우팅 완료

---
+++ front/src/component/LogIn.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function LogIn() {
+  return (
+    <div>
+      <Link to="/">
+        <h1>to home</h1>
+      </Link>
+      <h1>LogIn</h1>
+    </div>
+  );
+}
