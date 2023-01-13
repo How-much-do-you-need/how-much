577eff37f7155bccc68fb091e5e425a5d4357959
Dingadung <1229jimin@naver.com>
2023-01-13 19:47:28 +0900

    [Add] backend project initalizing

---
+++ backend/need-web/src/main/java/com/howmuch/needweb/NeedWebApplication.java
@@ -0,0 +1,13 @@
+package com.howmuch.needweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeedWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeedWebApplication.class, args);
+	}
+
+}
---
+++ backend/need-web/src/test/java/com/howmuch/needweb/NeedWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howmuch.needweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeedWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
