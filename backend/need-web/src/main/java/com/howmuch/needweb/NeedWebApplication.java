df1468c1e0473bb3d5ae531fdc18d6af05c3b700
Dingadung <1229jimin@naver.com>
2023-01-17 19:07:29 +0900

    [Add] 로그인, 회원 관련 파일 추가 데이터 베이스 테이블 하나 추가

--- backend/need-web/src/main/java/com/howmuch/needweb/NeedWebApplication.java
+++ backend/need-web/src/main/java/com/howmuch/needweb/NeedWebApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @EnableTransactionManagement
@@ -13,6 +16,12 @@ public class NeedWebApplication {
 	public static void main(String[] args) {
 		System.out.println("how much do you need project start!");
 		SpringApplication.run(NeedWebApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String welcome(@CookieValue(name = "id", defaultValue = "") String id, Model model) {
+		model.addAttribute("id", id);
+		return "main";
 	}
 
 }
