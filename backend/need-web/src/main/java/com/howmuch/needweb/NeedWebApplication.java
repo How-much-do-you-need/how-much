5f0e40848569f37beceb87be16ae82838ba21eed
Dingadung <1229jimin@naver.com>
2023-01-17 16:21:31 +0900

    [Feat] howdb 데이터베이스 연동

--- backend/need-web/src/main/java/com/howmuch/needweb/NeedWebApplication.java
+++ backend/need-web/src/main/java/com/howmuch/needweb/NeedWebApplication.java
@@ -2,11 +2,16 @@ package com.howmuch.needweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@Controller
+@EnableTransactionManagement
 @SpringBootApplication
 public class NeedWebApplication {
 
 	public static void main(String[] args) {
+		System.out.println("how much do you need project start!");
 		SpringApplication.run(NeedWebApplication.class, args);
 	}
 
