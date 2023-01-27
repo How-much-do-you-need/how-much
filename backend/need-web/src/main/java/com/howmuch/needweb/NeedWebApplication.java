537049f05d77d087fbd4e49dfcf1dc73d4d78af9
Dingadung <1229jimin@naver.com>
2023-01-27 21:10:12 +0900

    [Add]: 화면 테스트

--- backend/need-web/src/main/java/com/howmuch/needweb/NeedWebApplication.java
+++ backend/need-web/src/main/java/com/howmuch/needweb/NeedWebApplication.java
@@ -7,8 +7,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableTransactionManagement
 @SpringBootApplication
 public class NeedWebApplication {
